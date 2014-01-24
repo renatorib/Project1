2626a5e325e736762f53b5a57eaf80f2c415c75a
ThiagoCasao <thiagocasao@gmail.com>
2014-01-24 06:35:57 -0200

    Botão salvar

--- app/controllers/freights_controller.rb
+++ app/controllers/freights_controller.rb
@@ -4,11 +4,37 @@ class FreightsController < ApplicationController
 
 	end
 
-
 	def new
 		@freight = Freight.new(shipper: Shipper.first)
 		@origins = City.all
 		@destinations = City.all
 	end
+
+	def create
+		binding.pry
+		@freight = Freight.new(urgency: params[:urgency], price: params[:price], description: params[:description])
+		@freight.origin = City.find(params[:origin])
+		@freight.destination = City.find(params[:destination])
+		@freight.tracked = params[:tracked]
+		@freight.insured = params[:insured]
+		@freight.price = params[:heigth]
+		@freight.heigth = params[:heigth]
+		@freight.width = params[:width]
+		@freight.weigth = params[:weigth]
+		@freight.length = params[:length]
+		@freight.amount = params[:amount]
+		
+		if @freight.save
+			redirect_to(action: "index")
+		else
+			render :nothing
+		end 
+	end
+
+  private
+
+  def freight_params
+    params.require(:freight)
+  end
 
 end
--- spec/controllers/freights_controller_spec.rb
+++ spec/controllers/freights_controller_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe FreightsController do
 
+	it { should route(:get, '/freights').to(controller: 'freights', action: 'index') }
+
 	describe 'GET #new' do
     before { get :new }
     it { should render_template('new') }
   end
 
-	it { should route(:get, '/freights').to(controller: 'freights', action: 'index') }
-	
 end
